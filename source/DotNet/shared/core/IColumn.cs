1a0f0352fe7f795ee0e2cd42545a32a00f6f60f5
ereadian <ereadian@gmail.com>
2016-12-04 21:07:40 -0800

    Initial core

---
+++ source/DotNet/shared/core/IColumn.cs
@@ -0,0 +1,14 @@
+﻿//------------------------------------------------------------------------------------------------------------------------------------------ 
+// <copyright file="IColumn.cs" company="Ereadian"> 
+//     Copyright (c) Ereadian.  All rights reserved. 
+// </copyright> 
+//------------------------------------------------------------------------------------------------------------------------------------------ 
+
+namespace ereadian.dbdocgen.core
+{
+    public interface IColumn
+    {
+        ITable Table { get; set; }
+        string Name { get; set; }
+    }
+}
---
+++ source/DotNet/shared/core/IDataType.cs
@@ -0,0 +1,14 @@
+﻿//------------------------------------------------------------------------------------------------------------------------------------------ 
+// <copyright file="IColumn.cs" company="Ereadian"> 
+//     Copyright (c) Ereadian.  All rights reserved. 
+// </copyright> 
+//------------------------------------------------------------------------------------------------------------------------------------------ 
+
+namespace ereadian.dbdocgen.core
+{
+    public interface IDataType
+    {
+        string TypeName { get; set; }
+        bool RequireSize { get; set; }
+    }
+}
